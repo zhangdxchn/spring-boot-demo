59842eff9c2d47e3d7de7b1d7502fdfd042a6bee
zhangdx <zhangdxchn@gmail.com>
2016-11-21 23:32:41 +0800

    更新数据库脚本

--- doc/build_db.sql
+++ doc/build_db.sql
@@ -1,26 +1,15 @@
+# Create by zhangdx 2016-11-21.
+# Username: root
+# Password: admin123
+
+# Create DATABASE
 CREATE DATABASE `spring_boot_demo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-CREATE TABLE `tb_news` (
-  `id`          INT(11)    NOT NULL AUTO_INCREMENT,
-  `title`       VARCHAR(100)        DEFAULT NULL
-  COMMENT '标题',
-  `author`      VARCHAR(32)         DEFAULT NULL
-  COMMENT '新闻作者',
-  `content`     TEXT COMMENT '新闻正文',
-  `imageUrl`    VARCHAR(100)        DEFAULT NULL
-  COMMENT '新闻缩略图片',
-  `link`        VARCHAR(100)        DEFAULT NULL
-  COMMENT '原文链接',
-  `description` VARCHAR(2000)       DEFAULT NULL
-  COMMENT '新闻描述',
-  `time`        DATETIME   NOT NULL
-  COMMENT '新闻时间',
-  `updateTime`  DATETIME            DEFAULT NULL
-  COMMENT '上一次更新时间',
-  `isDelete`    TINYINT(4) NOT NULL DEFAULT '0'
-  COMMENT '是否删除: 0：false;（默认值） 1: true;',
-  `isShow`      TINYINT(4) NOT NULL DEFAULT '0'
-  COMMENT '是否对外发布：0 false (默认) ; 1 true',
+# User
+DROP TABLE  `tb_user` ;
+CREATE TABLE `tb_user` (
+  `id`   INT(11)  NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50)       DEFAULT NULL COMMENT '用户名',
+  `time` DATETIME NOT NULL COMMENT '时间',
   PRIMARY KEY (`id`)
-)
-  COMMENT = '新闻表';
+) COMMENT = '用户表';
